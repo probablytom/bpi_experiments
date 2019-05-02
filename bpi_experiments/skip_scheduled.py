3d6b18b9d8bc5121848e4377fd6732e3033fa415
Tom Wallis <github@tomwallis.net>
2019-05-02 15:38:38 +0100

    Made skip_scheduled a little more elegant

--- bpi_experiments/skip_scheduled.py
+++ bpi_experiments/skip_scheduled.py
@@ -1,16 +1,8 @@
 from bpi_13_experimental_frontend import run_model, skip_current_action
 
 class SkipLogAdvice(object):
-
-    def should_skip(self, attr):
-        '''
-        Skip all scheduling actions.
-        :return:  Bool indicating whether to activate.
-        '''
-        return 'schedule' in attr.func_name
-
     def prelude(self, attr, _):
-        if self.should_skip(attr):
+        if 'schedule' in attr.func_name:
             skip_current_action()
 
 run_model(SkipLogAdvice,
