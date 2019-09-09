b95b0aaff096a3247dfa6b07b02933b0898ad033
Tom Wallis <github@tomwallis.net>
2019-09-09 12:02:40 +0100

    Working models of competence etc

--- competence_model/competence.py
+++ competence_model/competence.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 from math import exp
 from drawer import AdviceBuilder
-from random import random
-from bpi_13_experimental_frontend import action_log, log_fuzz, company, new_trace, experimental_environment
+from bpi_13_experimental_frontend import action_log, log_fuzz, company, new_trace, SpecialistWorkflow, specialists, CustomerServiceActor, set_up_new_actor, SpecialistActor, reps
 from functools import partial
+from random import random, choice
 
 
 class ExperimentAdvice(object):
@@ -27,7 +27,19 @@ class ExperimentAdvice(object):
             ExperimentAdvice.invoked_count += not ExperimentAdvice.invoked  # Increment if it's not been invoked yet.
             ExperimentAdvice.invoked = True
 
-        attribute(*args, **kwargs)
+        if actor.mistake_to_make is not None and actor.mistake_to_make[0] == 'around' \
+                and attribute.func_name is not "END" and attribute.func_name is not "START":
+            func_to_run = actor.mistake_to_make[1]
+            fuzzing_name_label = actor.mistake_to_make[2]
+            func_to_run(attribute, actor, *args, **kwargs)
+
+            log_fuzz(fuzzing_name_label, attribute.func_name, actor.actor_name)
+
+            actor.mistake_to_make = None
+            ExperimentAdvice.invoked_count += not ExperimentAdvice.invoked  # Increment if it's not been invoked yet.
+            ExperimentAdvice.invoked = True
+        else:
+            attribute(*args, **kwargs)
 
         if actor.mistake_to_make is not None and actor.mistake_to_make[0] == 'after'\
                     and attribute.func_name is not "END" and attribute.func_name is not "START":
@@ -43,18 +55,23 @@ class ExperimentAdvice(object):
 
 
 class NewTasksOnEnd(object):
+
     def encore(self, _attribute, _actor, _result):
         company.recieve_message('start')
-        if not ExperimentAdvice.invoked:
-            action_log.pop()
-            if 'fuzzed tasks' in experimental_environment.keys() and experimental_environment['fuzzed tasks'][-1] == []:
-                experimental_environment['fuzzed tasks'].pop()
+        # if not ExperimentAdvice.invoked:
+        #     action_log.pop()
+        #     if 'fuzzed tasks' in experimental_environment.keys() and experimental_environment['fuzzed tasks'][-1] == []:
+        #         experimental_environment['fuzzed tasks'].pop()
         new_trace()
         ExperimentAdvice.invoked = False
 
 
 def remove_previous_action(_, __):
     action_log[-1].pop()
+
+
+def repeat_step(_, __):
+    action_log[-1].append(action_log[-1][-1])
 
 
 mistake_points = []
@@ -90,9 +107,34 @@ class CompetenceModel(object):
         if not hasattr(actor, 'last_mistake_made'):
             setattr(actor, 'last_mistake_made', 0)
         if not hasattr(actor, 'carefulness'):
-            setattr(actor, 'carefulness', 3)
+            setattr(actor, 'carefulness', 10)
         if not hasattr(actor, 'mistake_to_make'):
             setattr(actor, 'mistake_to_make', None)
+        if not hasattr(actor, 'number_above_permissions'):
+            setattr(actor, 'number_above_permissions', 0)
+        if not hasattr(actor, 'retirement_exp'):
+            setattr(actor, 'retirement_exp', 2 * self.learning_point - round(random() * self.learning_point * choice([-1,1])) )
+        if not hasattr(actor, 'retired'):
+            setattr(actor, 'retired', False)
+
+        if sum(actor.experiences) > actor.retirement_exp:
+
+            # When retired, we spawn a replacement actor.
+            if not actor.retired:
+                actor.retired = True
+                # Check to see what kind of actor we need to replace
+                isSpecialist = actor.actor_name[0] == 'S'
+                if isSpecialist:
+                    set_up_new_actor(SpecialistActor, specialists)
+                else:
+                    set_up_new_actor(CustomerServiceActor, reps)
+
+                # Add to the relevant troupes (and the global clock)!
+
+
+
+            return actor.idle
+
 
         # If the actor has previously generated a new task, with a
         #  ...cost via `au`, we count the experiences of the task now.
@@ -121,7 +163,14 @@ class CompetenceModel(object):
                 actor.last_mistake_made = sum(actor.experiences)
 
                 # Set the actor to actually make a mistake
-                actor.mistake_to_make = ('after', remove_previous_action, 'skip step')
+                if percentage_competence < 0.5:
+                    actor.mistake_to_make = ('after', repeat_step, 'repeat step')
+                else:
+                    if isinstance(actor, CustomerServiceActor):
+                        actor.number_above_permissions += 1
+                    else:
+                        actor.mistake_to_make = ('after', remove_previous_action, 'skip step')
+
 
                 # Plotting, can safely remove
                 if actor.actor_name == 'Customer Service Actor 5' and CompetenceModel.show_graphs:
@@ -135,13 +184,51 @@ class CompetenceModel(object):
 
 
         # * Run the actual attribute (in our case, `get_next_task`)
-        curr_task = attribute(*args, **kwargs)
+        if actor.number_above_permissions > 0:
+            if specialists not in actor.troupes:
+                specialists.add_member(actor)
+            curr_task = attribute(*args, **kwargs)
+            if type(curr_task) is str:
+                actor.number_above_permissions -= 1
+                for name, method in SpecialistWorkflow.__dict__.items():
+                    if name.lower() == curr_task:
+                        curr_task = partial(method, actor)
+                        task_name = name
+
+                # Log the abnormality
+                log_fuzz('acting above permissions', task_name, actor.actor_name)
+
+
+            actor.troupes.remove(specialists)
+
+        else:
+            curr_task = attribute(*args, **kwargs)
 
         # * Encore
         actor.curr_task = curr_task
 
         # We have to return the value from the task
         return curr_task
+
+# class ActAbovePermissions(object):
+#     def around(self, attribute, actor, *args, **kwargs):
+#         '''
+#
+#         :param actor:
+#         :param attribute:
+#         :param args:
+#         :param kwargs:
+#         :return:
+#         '''
+#         if not hasattr(actor, 'mistake_to_make'):
+#             setattr(actor, 'mistake_to_make', None)
+#
+#         if actor.mistake_to_make is not None and actor.mistake_to_make[2] == 'acting outside permissions':
+#
+#         else:
+#             curr_task = attribute(*args, **kwargs)
+#
+#         return curr_task
 
 
 def basic_sigmoid(genius=0.1, learning_point = 50):
--- competence_model/test_competence_application.py
+++ competence_model/test_competence_application.py
@@ -1,8 +1,11 @@
 import unittest
 from bpi_13_experimental_frontend import run_model_advanced_aspect_application as run_model
 from bpi_13_experimental_frontend import construct_universe, action_log, generate_CSV, generate_XES
-from bpi_13_experimental_frontend import reset_universe, new_trace, experimental_environment
+from bpi_13_experimental_frontend import reset_universe, new_trace, experimental_environment, reset_traces
+import bpi_13_experimental_frontend
 from competence import CompetenceModel, ExperimentAdvice, NewTasksOnEnd
+from functools import partial
+from Queue import Queue
 
 att = 0.7
 intro = 0.5
@@ -43,26 +46,64 @@ class TestCompetence(unittest.TestCase):
     def test_multiple_traces_consistent_actors(self):
 
         construct_universe()
-        ticks = 250
-        run_model(([
-            (ExperimentAdvice, 'all workflows', list(), dict()),
-            (CompetenceModel, 'get_next_task', [0.05, ticks/2], dict()),
-            (NewTasksOnEnd, 'end', list(), dict()),
-        ]),
-            num_start_messages=1,
-            num_ticks=ticks,
-        )
+        ticks = 350
+        iterations = 5
+        simulate = partial(run_model,
+                           ([
+                               # (ActAbovePermissions, 'get_next_task', list(), dict()),
+                               (CompetenceModel, 'get_next_task', [0.05*iterations, ticks/2/iterations], dict()),
+                               (ExperimentAdvice, 'all workflows', list(), dict()),
+                               (NewTasksOnEnd, 'end', list(), dict()),
+                           ]),
+                           num_start_messages=1,
+                           num_ticks=ticks
+                           )
+        # run_model(([
+        #     # (ActAbovePermissions, 'get_next_task', list(), dict()),
+        #     (CompetenceModel, 'get_next_task', [0.05*iterations, ticks/2/iterations], dict()),
+        #     (ExperimentAdvice, 'all workflows', list(), dict()),
+        #     (NewTasksOnEnd, 'end', list(), dict()),
+        # ]),
+        #     num_start_messages=1,
+        #     num_ticks=ticks,
+        # )
+
+        self.play_simulation(simulate, discard_work=True)
+        self.play_simulation(simulate, discard_work=False)
 
         # We also stopped halfway through a simulation...so unless the last action in the last action log is one the simulation should end on, we should remove that trace (and its fuzzing effects)
-        if action_log[-1][-1] != "A_declined" \
-            and action_log[-1][-1] != "W_beoordelen_fraude_complete" \
-            and action_log[-1][-1] != "W_wijzigen_contractgegevens_schedule":
-            action_log.pop()
-            if len(experimental_environment["fuzzed tasks"]) != len(action_log):
-                experimental_environment["fuzzed tasks"].pop()
+        try:
+            if action_log[-1][-1] != "A_declined" \
+                and action_log[-1][-1] != "W_beoordelen_fraude_complete" \
+                and action_log[-1][-1] != "W_wijzigen_contractgegevens_schedule":
+                action_log.pop()
+                if len(experimental_environment["fuzzed tasks"]) != len(action_log):
+                    experimental_environment["fuzzed tasks"].pop()
+        except:
+            pass
 
         generate_XES()
         generate_CSV()
+
+    def play_simulation(self, simulation_function, discard_work=True, *args, **kwargs):
+        '''
+        Runs through the simulation but discards the results of the work
+        (so no activity is recorded, but the actors learn, retire, etc.)
+        :param discard_work: Dictates whether the work done playing through this simulation should be discarded, so actors simply learn.
+        :return:
+        '''
+        simulation_function(*args, **kwargs)
+        # reset_traces() if discard_work else None
+        if discard_work:
+            [bpi_13_experimental_frontend.action_log.pop() for _ in range(len(bpi_13_experimental_frontend.action_log))]
+            bpi_13_experimental_frontend.action_log.append([])
+            experimental_environment.__delitem__("fuzzed tasks")
+
+            # Remove all pending tasks
+            bpi_13_experimental_frontend.company.troupe_work_queue = Queue()
+            bpi_13_experimental_frontend.reps.troupe_work_queue = Queue()
+            bpi_13_experimental_frontend.specialists.troupe_work_queue = Queue()
+
 
 
 if __name__ == '__main__':
